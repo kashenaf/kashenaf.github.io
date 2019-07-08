7a39f28646cba3e56a7d5d04a6250f844fe73fe3
Kalkidan Ashenafi <kashenaf@gmu.edu>
2019-07-08 09:56:57 -0400

    intial commit

---
+++ 5_2_Principal-Final(1) (Published)/data/slide29.js
@@ -0,0 +1,5 @@
+(function(){var loadHandler=window['sl_{BFD63969-E730-4D15-8345-0295936854DC}'];loadHandler&&loadHandler(28, '<div id="spr0_14fba7fb"><div id="spr1_14fba7fb" class="kern slide"><img id="img2_14fba7fb" src="data/img2.png" width="720px" height="405px" alt="" style="left:0px;top:0px;"/><div id="spr3_14fba7fb" style="top:389.865px;"><div style="width:0px;"><span id="txt0_14fba7fb" class="nokern" data-width="144.298828" style="left:20.389px;top:-0.85px;">Classification: Company Confidential</span></div></div><div id="spr4_14fba7fb" style="left:624.446px;top:373.682px;"><img id="img0_14fba7fb" src="data/img1.png" width="81" height="24" alt="Picture 6" style="left:0.18px;top:0.143px;"/></div></div><div id="spr2_14fba7fb" class="kern slide"><div id="spr5_14fba7fb" style="left:15px;top:377.874px;"><div style="width:0px;"><span id="txt1_14fba7fb" class="nokern" data-width="9.123047" style="top:-0.938px;">29</span></div></div><div id="spr6_14fba7fb"><img id="img1_14fba7fb" src="data/img53.png" width="720" height="405" alt="一張含有 螢幕擷取畫面 的圖片\
+\
+\
+\
+自動產生的描述"/></div></div></div>');})();
